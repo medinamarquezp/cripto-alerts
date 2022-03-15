8a530f37a14907d59816bdd64d0edcd1714064ac
Pedro Medina Márquez <medinamarquezp@gmail.com>
2022-03-15 23:55:43 +0100

    feat: custom transporter and mailer result

---
+++ src/services/notifications/interfaces/mailer-result.interface.ts
@@ -0,0 +1,5 @@
+export interface IMailerResult {
+  messageSended: boolean,
+  messageId?: string,
+  errorMessage?: string;
+}
--- src/services/notifications/services/mailer.service.ts
+++ src/services/notifications/services/mailer.service.ts
@@ -1,10 +1,11 @@
-import { createTransport } from "nodemailer";
+import { createTransport, Transporter } from "nodemailer";
 import { mailer } from "@/src/config";
 import { logger } from "@/src/shared/logger";
 import { template } from "../templates/base-notification.template";
-import { IMailerNotification } from "../dtos/mailer-notification.dto";
+import { IMailerNotification } from "../interfaces/mailer-notification.interface";
+import { IMailerResult } from "../interfaces/mailer-result.interface";
 
-const transporter = createTransport({
+const baseTransporter = createTransport({
   host: mailer.HOST,
   port: mailer.PORT,
   secure: false,
@@ -14,18 +15,21 @@ const transporter = createTransport({
   }
 });
 
-export const sendEmail = async (message: IMailerNotification): Promise<void> => {
+export const sendEmail = async (message: IMailerNotification, customTransporter?: Transporter): Promise<IMailerResult> => {
+  const transporter = customTransporter ? customTransporter : baseTransporter;
   const { to, subject, content } = message;
   try {
     const email = await transporter.sendMail({
       from: mailer.FROM,
       to: `${to}, ${to}`,
       subject,
-      html: template(content),
+      html: template(content)
     });
     logger.info(`Email with subject "${subject}" has been send to "${to}" with message id "${email.messageId}"`);
+    return { messageSended: true, messageId: email.messageId };
   } catch (err) {
     const error = err as Error;
     logger.error(`Error on sending email to "${to}" with subject "${subject}": ${error.message}`);
+    return { messageSended: false, errorMessage: error.message };
   }
 };
